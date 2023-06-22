5f31013d804c664260a0cfb992c511a1b2334d0f
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-06-22 14:04:54 +0300

    icons done

--- src/components/Profile/ProfileInfo/Contact/Contact.jsx
+++ src/components/Profile/ProfileInfo/Contact/Contact.jsx
@@ -2,41 +2,19 @@ import React from "react";
 import c from './Contact.module.scss'
 
 const Contact = ({ contactLink, contactName }) => {
-    let classIcon
-    switch (contactName) {
-        case "facebook":
-            classIcon = 'contact__icon_type_facebook'
-            break
-        case "website":
-            classIcon = 'contact__icon_type_website'
-            break
-        case "vk":
-            classIcon = 'contact__icon_type_vk'
-            break
-        case "twitter":
-            classIcon = 'contact__icon_type_twitter'
-            break
-        case "instagram":
-            classIcon = 'contact__icon_type_instagram'
-            break
-        case "youtube":
-            classIcon = 'contact__icon_type_youtube'
-            break
-        case "github":
-            classIcon = 'contact__icon_type_github'
-            break
-        case "mainLink":
-            classIcon = 'contact__icon_type_mainLink'
-            break
-        default:
-            classIcon = 'contact__icon_type_none'
-            break
-    }
 
     return (
         <li className={`${c.contact} ${contactLink ? c.contact_visible : ''}`}>
-            <label for="link"><div className={`${c.contact__icon} ${c[classIcon]}`}></div></label>
-            <a id="link" href={contactLink} className={c.contact__link}>{contactName}</a>
+            <a href={contactLink} className={c.contact__link}>
+                {contactName === "youtube" && <div className={`${c.contact__icon} ${c.contact__icon_type_youtube}`}></div>}
+                {contactName === "facebook" && <div className={`${c.contact__icon} ${c.contact__icon_type_facebook}`}></div>}
+                {contactName === "website" && <div className={`${c.contact__icon} ${c.contact__icon_type_website}`}></div>}
+                {contactName === "instagram" && <div className={`${c.contact__icon} ${c.contact__icon_type_instagram}`}></div>}
+                {contactName === "vk" && <div className={`${c.contact__icon} ${c.contact__icon_type_vk}`}></div>}
+                {contactName === "twitter" && <div className={`${c.contact__icon} ${c.contact__icon_type_twitter}`}></div>}
+                {contactName === "github" && <div className={`${c.contact__icon} ${c.contact__icon_type_github}`}></div>}
+                {contactName === "mainLink" && <div className={`${c.contact__icon} ${c.contact__icon_type_mainLink}`}></div>}
+            </a>
         </li >
     )
 }
