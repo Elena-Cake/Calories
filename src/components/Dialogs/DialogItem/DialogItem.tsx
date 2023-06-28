42c41f01ab4bde8c98634fcda4f785fa6a62479d
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-06-28 13:34:38 +0300

    dialog item edit to ts

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import s from './DialogItem.module.css'
 import { NavLink } from "react-router-dom";
 
+type propsType = { name: string, id: number, isActive: boolean }
 
-
-const DialogItem = ({ name, id, isActive }) => {
+const DialogItem: React.FC<propsType> = ({ name, id, isActive }) => {
     return (
         <div className={s.dialogs__item + ' ' + (isActive ? s.dialogs__item_active : '')}>
             <NavLink to={"/dialogs/" + id} className={s.dialogs__item_link}>
